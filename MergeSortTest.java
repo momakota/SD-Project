d5e4f3dd079c3595538e7a6fe48ad5431b9c972f
Maureen KP Makota <MM4118@live.mdx.ac.uk>
2025-04-17 17:48:15 +0100

    Uploaded the required packages and adjusted the test code

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -1,10 +1,24 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MergeSortTest {
+    
     @Test
     public static void test() {
-        assertEquals(MergeSort.merge)
+        
+        ArrayList<String> input = new ArrayList<>(Arrays.asList("January",
+        "February", "March", "April", "May", "June", "July", "August",
+        "September", "October", "November", "December"));
+        
+        ArrayList<String> expectedOutput = new ArrayList<>(Arrays.asList("April",
+        "August", "December", "February", "January", "July", "June", "March",
+        "May", "November", "October", "September"));
+        
+        MergeSort.mergeSort(input);
+        assertEquals(input, expectedOutput);
+        
     }
 }
